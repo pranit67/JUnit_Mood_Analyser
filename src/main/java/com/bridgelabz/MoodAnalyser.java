4bb90d57b49839488e7b952e17b0f7cb83e566e7
pranit67 <pranit67@gmail.com>
2023-01-18 23:38:25 +0530

    Added:Test Case to Analyse and Respond Happy or Sad Mood

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Mood Analyser Program.");
+    //    public static void main(String[] args) {
+//        System.out.println("Welcome to Mood Analyser Program.");
+//}
+    public String analyseMood(String message) {
+        if (message.contains("Sad")) {
+
+            return "SAD";
+        } else {
+            return "HAPPY";
+        }
+
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,4 +1,19 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyserTest {
+    @Test
+    public void givenMessage_whenSad_shouldReturnSadMood() {
+        MoodAnalyser obj = new MoodAnalyser();
+        String mood = obj.analyseMood("I am in Sad Mood");
+        Assert.assertEquals("SAD", mood);
+    }
+    @Test
+    public void givenMessage_whenAny_shouldReturnHappyMood() {
+        MoodAnalyser obj = new MoodAnalyser();
+        String mood = obj.analyseMood("I am in Any Mood");
+        Assert.assertEquals("HAPPY", mood);
+    }
 }
